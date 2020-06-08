9da4dc3e25047adc7ae683f5a57b21d28dfb9243
Megan Hepner <megan.hepner@gmail.com>
2020-06-08 11:56:37 -0700

    added jquery code to scripts to add flavors in HTML
    Co-authored-by: Matt McFarland <mrmcfarland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 //webpage that will list flavors
 //create javascript loop of flavors
 //use JS to insert flavors into HTML from array
+
+$(document).ready(function() {
+  const flavors = ['cookies and cream', 'vanilla', 'chocolate', 'rocky road', 'green tea']
+  flavors.forEach(function(flavor){
+    $('#sentence').append(`<li>${flavor}</li>`);
+  });
+});
